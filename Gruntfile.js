8cd3bb8c03e0b7787466c30a5e2b57ff243b2df8
Will Jones <willjones@ua.edu>
2015-03-12 14:52:52 -0500

    Added uglify and bump grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,14 @@ module.exports = function(grunt){
                 module: 'ui.components.templates'
             }
         },
+        uglify:{
+            dist: {
+                files: [{
+                    src: ['dist/ui-component*.js'],
+                    dest: 'dist/ui-components.min.js'
+                }]
+            }
+        },
         less: {
             components:{
                 files: {
@@ -44,14 +52,35 @@ module.exports = function(grunt){
             kss: {
                 command: 'kss-node src styleguide --template kss-template --helpers kss-template/helpers --custom codetemplate --custom hidemarkup'
             }
+        },
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                updateConfigs: ['pkg'],
+                commit: false,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json', 'bower.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-html2js');
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-bump');
 
     // Default task
     grunt.registerTask('default', ['html2js', 'less', 'concat', 'exec']);
+    grunt.registerTask('build', ['default', 'uglify']);
 };
